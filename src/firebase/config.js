02639fa6e2f4f70ee03824afc0d102039bb0b4b1
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-02-07 05:41:46 +0530

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwnp96WhNNtpgMmKrBrrdmJgjvSaB8Dis",
+    authDomain: "finacetracker-a6980.firebaseapp.com",
+    projectId: "finacetracker-a6980",
+    storageBucket: "finacetracker-a6980.appspot.com",
+    messagingSenderId: "643879980844",
+    appId: "1:643879980844:web:4a7ff628b44efe7e02e689",
+    measurementId: "G-KHBXHM4045"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
