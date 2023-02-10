924b22c0fdc38b5b3882e1612d8d8d882170ea80
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-02-10 07:48:52 +0530

    delete function added

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from './Home.css';
+import { useFirestore } from "../../hooks/useFirebase";
 
 const TransactionList = ({ transactions }) => {
+    const { deleteDocument } = useFirestore('transactions')
 
     return (
         <ul className={'transactions'}>
@@ -9,6 +11,7 @@ const TransactionList = ({ transactions }) => {
                 <li key={transaction.id}>
                     <p className={styles.name}>{transaction.name}</p>
                     <p className={styles.amount}>${transaction.amount}</p>
+                    <button onClick={() => deleteDocument(transaction.id)}>x</button>
                 </li>
             ))}
         </ul>
