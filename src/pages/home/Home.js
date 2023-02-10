fcaf8e6e0c7bae7bab12522afbbeb7f1cfb3d90a
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-02-10 07:43:57 +0530

    fetching document to firebase added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,21 @@
 import React from "react";
 import TransactionForm from "./TransactionForm";
-import { useAuthContext } from "../../hooks/useAuthContext";
-
+import { useCollection } from '../../hooks/useCollection';
 import './Home.css';
+import TransactionList from "./TransactionList";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const Home = () => {
     const { user } = useAuthContext();
+    const { documents, error } = useCollection(
+        'transactions', ["uid", "==", user.uid], ['createdAt', 'desc']
+    )
 
     return(
         <div className={'container'}>
             <div className={'content'}>
-                list
+                { error && <p>{ error }</p> }
+                { documents && <TransactionList transactions={documents} /> }
             </div>
             <div className={'sidebar'}>
                 <TransactionForm uid={user.uid}/>
---
+++ src/pages/home/TransactionList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './Home.css';
+
+const TransactionList = ({ transactions }) => {
+
+    return (
+        <ul className={'transactions'}>
+            {transactions.map((transaction) => (
+                <li key={transaction.id}>
+                    <p className={styles.name}>{transaction.name}</p>
+                    <p className={styles.amount}>${transaction.amount}</p>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default TransactionList;
